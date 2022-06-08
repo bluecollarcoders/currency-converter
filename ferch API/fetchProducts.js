3e322128ad001ad96de75279d1c9864fc8154d6d
Caleb Matteis <matteismusic@gmail.com>
2022-06-08 13:21:00 -0400

    commit

---
+++ ferch API/fetchProducts.js
@@ -0,0 +1,19 @@
+async function fetchProducts() {
+  try {
+    const response = await fetch(
+      "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(`Could not get products: ${error}`);
+  }
+}
+
+const jsonPromise = fetchProducts();
+jsonPromise.then((json) => console.log(json[0].name));
+
+fetchProducts();
